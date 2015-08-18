4515745a5af220f0ffa6654799fad1062574818b
Yuri Shewchuk <ycshewchuk@gmail.com>
2015-08-18 02:50:32 -0400

    initial test of template
    - also minimize main index (manually updated)
    - split template and data
    - manually initialize angular
    - replace script.js with LazyLoad for css capabilities

---
+++ resume.js
@@ -0,0 +1,16 @@
+﻿angular.module('resume', [])
+    .controller('ResumeCtrl', ['$scope', '$http', function ($scope, $http)
+    {
+        $http.get('person.json').then(function (response) { $scope.person = response.data; });
+    }])
+    .directive('header', function ()
+    {
+        return {
+            templateUrl: 'header.html'
+        };
+    });
+
+document.initialize.push(function ()
+{
+    angular.bootstrap(document, ['resume']);
+});
